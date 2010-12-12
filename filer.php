20fec2bdbad7e0ba532b3018728d70a9626fdfa9
Ahmed Nuaman, Freelance Designer and Developer <ahmed@ahmednuaman.com>
2010-12-12 21:38:54 +0000

    added copyright statement

--- filer.php
+++ filer.php
@@ -8,6 +8,15 @@ Author: Ahmed Nuaman
 Author URI: http://www.ahmednuaman.com
 */
 
+/**
+ * @author			Ahmed Nuaman (http://www.ahmednuaman.com)
+ * @langversion		5
+ * 
+ * This work is licenced under the Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License. 
+ * To view a copy of this licence, visit http://creativecommons.org/licenses/by-sa/2.0/uk/ or send a letter 
+ * to Creative Commons, 171 Second Street, Suite 300, San Francisco, California 94105, USA.
+*/
+
 error_reporting( E_ALL ^ E_NOTICE );
 
 add_action( 'wp_ajax_get_blog_file', 				'filer_get_blog_file' );
